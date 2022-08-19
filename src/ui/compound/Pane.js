5598f33b229b28709281c2ae42f6343adb3c4c6e
Luis Vasquez <luisvasq@mit.edu>
2022-08-19 23:57:13 +0200

    Fix `Pane` styling (solves #18)

--- src/ui/compound/Pane.js
+++ src/ui/compound/Pane.js
@@ -8,53 +8,48 @@ import { APP_BAR_WIDTH, PANE_WIDTH } from '../../utils/constants'
 import { forceArray } from '../../utils'
 
 const styles = {
-  titleDiv: {
+  drawer: {
+    '& .MuiDrawer-paper': {
+      left: `${APP_BAR_WIDTH + 1}px`,
+      height: '100%',
+      overflow: 'hidden',
+    },
+  },
+  content: {
+    p: 2.5,
+    height: '100%',
+    maxWidth: (theme) =>
+      `calc(100vw - ${APP_BAR_WIDTH + 1}px - ${theme.spacing(5)})`,
+    overflow: 'auto',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'center',
+    mb: 1,
+    p: 2.5,
     fontSize: '25px',
     borderColor: 'text.secondary',
     borderBottom: '2px',
-    pb: 2,
-    mb: 3,
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
   },
-  titleText: {
+  title: {
     ml: 0.5,
-    mr: 0.5,
-  },
-  rightButton: {
-    ml: 'auto',
     mr: 0.5,
   },
   leftButton: {
     ml: 0.5,
     mr: 'auto',
   },
+  rightButton: {
+    ml: 'auto',
+    mr: 0.5,
+  },
 }
 
-const getDrawerStyle = (width) => ({
-  '& .MuiDrawer-paper': (theme) => ({
-    width,
-    maxWidth: `calc(100vw - ${APP_BAR_WIDTH + 1}px - ${theme.spacing(5)})`,
-    left: `${APP_BAR_WIDTH + 1}px`,
-    height: `calc(100vh - ${theme.spacing(5)})`,
-    p: 2.5,
-    overflow: 'auto',
-  }),
-})
-
-const PaneRoot = ({
-  disabled,
-  elevation,
-  open,
-  width = PANE_WIDTH,
-  sx = [],
-  ...props
-}) => (
+const PaneRoot = ({ disabled, elevation = 3, open, sx = [], ...props }) => (
   <Drawer
-    sx={[getDrawerStyle(width), ...forceArray(sx)]}
+    sx={[styles.drawer, ...forceArray(sx)]}
     anchor="left"
-    variant={open ? 'permanent' : 'persistent'} // "persistent"
+    variant={open ? 'permanent' : 'persistent'} // 'temporary'
     {...{ disabled, elevation, open, ...props }}
   />
 )
@@ -62,7 +57,6 @@ PaneRoot.propTypes = {
   disabled: PropTypes.bool,
   elevation: PropTypes.oneOf([...Array(25).keys()]),
   open: PropTypes.bool,
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   sx: PropTypes.oneOfType([
     PropTypes.arrayOf(
       PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
@@ -70,7 +64,6 @@ PaneRoot.propTypes = {
     PropTypes.func,
     PropTypes.object,
   ]),
-  children: PropTypes.node,
 }
 
 const PaneHeader = ({
@@ -83,9 +76,9 @@ const PaneHeader = ({
 }) => {
   const icon = <FetchedIcon {...{ iconName }} />
   return (
-    <Box sx={[styles.titleDiv, ...forceArray(sx)]} {...props}>
+    <Box sx={[styles.header, ...forceArray(sx)]} {...props}>
       <Box sx={styles.leftButton}>{leftButton}</Box>
-      <Box sx={styles.titleText}>
+      <Box sx={styles.title}>
         {title} {icon}
       </Box>
       <Box sx={styles.rightButton}>{rightButton}</Box>
@@ -112,17 +105,21 @@ const Pane = ({
   iconName,
   leftButton,
   rightButton,
+  width = PANE_WIDTH,
   open,
   children,
   ...props
 }) => (
   <PaneRoot open={!!open} {...props}>
     <PaneHeader title={name} {...{ iconName, leftButton, rightButton }} />
-    <Box sx={{ minWidth: 'max-content' }}>{children}</Box>
+    <Box sx={[{ width }, styles.content]}>
+      <Box sx={{ minWidth: 'max-content' }}>{children}</Box>
+    </Box>
   </PaneRoot>
 )
 Pane.propTypes = {
   name: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   children: PropTypes.node,
 }
 
