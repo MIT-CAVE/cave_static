823cd6ca5e5c702c74b7666e47951f195756ff60
Jean Ghislain Billa <jeanbilla@dhcp-10-29-73-200.dyn.mit.edu>
2023-01-22 23:27:28 -0500

    Refactor Snackbar #123

--- src/ui/views/common/SnackBar.js
+++ src/ui/views/common/SnackBar.js
@@ -1,18 +1,28 @@
 import { Snackbar, IconButton } from '@mui/material'
-import React, { useState, Fragment } from 'react'
+import * as R from 'ramda'
+import React, { Fragment } from 'react'
 import { AiFillCloseCircle } from 'react-icons/ai'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { selectMessages } from '../../../data/selectors'
 import { removeMessage } from '../../../data/utilities/messagesSlice'
 
-const SnackBar = ({ message, messageKey, duration }) => {
-  const [open, setOpen] = useState(true)
+const SnackBar = () => {
+  const messages = useSelector(selectMessages)
   const dispatch = useDispatch()
+  const filterMessages = R.filter(R.prop('snackbarShow'))
+
+  const filteredMessages = filterMessages(messages)
+  const keys = R.keys(filteredMessages)
 
   const handleClose = () => {
-    dispatch(removeMessage({ messageKey: messageKey }))
-    setOpen(false)
+    dispatch(removeMessage({ messageKey: keys[0] }))
   }
+
+  const calculateDuration = R.pipe(
+    R.propOr(null, 'duration'),
+    R.unless(R.isNil, R.multiply(1000))
+  )
 
   const ClosingButton = (
     <Fragment>
@@ -29,15 +39,20 @@ const SnackBar = ({ message, messageKey, duration }) => {
   let vertical = 'bottom'
   let horizontal = 'center'
 
-  return (
-    <Snackbar
-      open={open}
-      message={message}
-      action={ClosingButton}
-      autoHideDuration={duration}
-      anchorOrigin={{ vertical, horizontal }}
-      onClose={handleClose}
-    />
+  return R.isEmpty(filteredMessages) ? (
+    []
+  ) : (
+    <div>
+      <Snackbar
+        open={true}
+        message={filteredMessages[keys[0]].message}
+        messageKey={keys[0]}
+        duration={calculateDuration(filteredMessages[keys[0]])}
+        anchorOrigin={{ vertical, horizontal }}
+        action={ClosingButton}
+      ></Snackbar>
+    </div>
   )
 }
+
 export default SnackBar
