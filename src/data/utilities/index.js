5c9bba25186bcf38ef8035bc084bb86923b65cc8
Connor Makowski <connorman528@gmail.com>
2023-02-02 17:09:36 -0500

    First pass at getting loading working static side

--- src/data/utilities/index.js
+++ src/data/utilities/index.js
@@ -1,3 +1,4 @@
+import loadingReducer from './loadingSlice'
 import messagesReducer from './messagesSlice'
 import sessionsReducer from './sessionsSlice'
 import tokensReducer from './tokensSlice'
@@ -5,6 +6,7 @@ import tokensReducer from './tokensSlice'
 import { combineReducers } from '../../utils'
 
 const utilitiesSlice = combineReducers({
+  loading: loadingReducer,
   messages: messagesReducer,
   sessions: sessionsReducer,
   tokens: tokensReducer,
