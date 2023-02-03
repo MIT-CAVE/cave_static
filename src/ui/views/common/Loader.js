31a9df8d463851e4a8b24d064e1da3dc23c8b257
mrkwanzaa <sledgeguter@gmail.com>
2023-02-03 11:59:46 -0500

    Fix loader flashing on quick loads

--- src/ui/views/common/Loader.js
+++ src/ui/views/common/Loader.js
@@ -1,7 +1,12 @@
 import { Backdrop, CircularProgress } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import { selectShowLoading } from '../../../data/selectors'
+import {
+  selectDataLoading,
+  selectIgnoreLoading,
+  selectSessionLoading,
+} from '../../../data/selectors'
 
 const style = {
   color: '#fff',
@@ -9,7 +14,38 @@ const style = {
 }
 
 const Loader = () => {
-  const open = useSelector(selectShowLoading)
+  const sessionLoading = useSelector(selectSessionLoading)
+  const dataLoading = useSelector(selectDataLoading)
+  const ignoreLoading = useSelector(selectIgnoreLoading)
+
+  const waitTimeout = useRef(0)
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    // show for ignoreloading
+    if (ignoreLoading) setOpen(true)
+    // start a timer if other loadings
+    else if (dataLoading || sessionLoading) {
+      if (waitTimeout.current === 0)
+        waitTimeout.current = setTimeout(() => setOpen(true), 3000)
+    } else {
+      // if loadings false, clear timer
+      if (waitTimeout.current !== 0) {
+        clearTimeout(waitTimeout.current)
+        waitTimeout.current = 0
+      }
+      // and close loader
+      if (open) setOpen(false)
+    }
+    // cleanup timer on unmount
+    return () => {
+      if (waitTimeout.current !== 0) {
+        clearTimeout(waitTimeout.current)
+        waitTimeout.current = 0
+      }
+    }
+  }, [dataLoading, ignoreLoading, open, sessionLoading])
+
   return (
     <Backdrop sx={style} open={open}>
       <CircularProgress color="inherit" />
