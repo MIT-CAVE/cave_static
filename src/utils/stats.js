d7aa7f5fe57c8d4bca32d4e693ba4d85dd7ff9dc
Connor Makowski <connorman528@gmail.com>
2023-02-15 14:49:46 -0500

    Flesh out stats better

--- src/utils/stats.js
+++ src/utils/stats.js
@@ -2,7 +2,7 @@ import * as R from 'ramda'
 
 // Calculate the mode item in a list (output formatted as a string)
 // This can mix strings and numbers, but will always return a string (of the number) or NaN
-// Note: 1 = '1' in this case
+// Note: 1 is equivalent to '1' in this case
 // EG: getMode([1, 2, 3, 1]) => '1'
 // EG: getMode([1, 'b','b','a','c']) => 'b'
 export const getMode = (arr) => {
@@ -15,38 +15,76 @@ export const getMode = (arr) => {
 }
 
 // Calculate the max item in a list (returns a number, string or NaN)
-// This should not mix strings and numbers (if so results may be unexpected and are based on ordering)
-// If the list is or only contains strings, returns NaN
-// EG: getMax([1, 2, 3, 1]) => 3
+// Note: Numbers are bigger than strings (1 > 'a')
+// Note: Lowercase is bigger than Uppercase ('a' > 'A')
+// Note: Uppercase is bigger than string numbers ('A' > '1')
+// EG: getMax([1, 2, 3]) => 3
+// EG: getMax(['a', 'b', 'c']) => 'c'
+// EG: getMax(['a', 'b', 'c', 'A']) => 'c'
+// EG: getMax(['a', 'b', 'c', 'A', 1]) => 'c'
 export const getMax = (arr) => {
-  const out = R.reduce(R.max, -Infinity, arr)
-  return out === -Infinity ? NaN : out
+  return R.pipe(
+    R.groupBy(R.type),
+    R.cond([
+      [
+        R.pipe(R.prop('Number'), R.length, R.flip(R.gt)(0)),
+        R.pipe(R.prop('Number'), R.reduce(R.max, -Infinity)),
+      ],
+      [
+        R.pipe(R.prop('String'), R.length, R.flip(R.gt)(0)),
+        R.pipe(R.prop('String'), R.reduce(R.max, '')),
+      ],
+      [R.T, R.always(NaN)],
+    ])
+  )(arr)
 }
 
 // Calculate the min item in a list (returns a number, string or NaN)
-// This should not mix strings and numbers (if so results may be unexpected and are based on ordering)
-// If the list is or only contains strings, returns NaN
-// EG: getMin([1, 2, 3, 1]) => 1
+// Note: Numbers are bigger than strings (1 > 'a')
+// Note: Lowercase is bigger than Uppercase ('a' > 'A')
+// Note: Uppercase is bigger than string numbers ('A' > '1')
+// EG: getMin([1, 2, 3]) => 1
+// EG: getMin(['a', 'b', 'c']) => 'a'
+// EG: getMin(['a', 'b', 'c', 'A']) => 'A'
+// EG: getMin(['a', 'b', 'c', 'A', 1]) => 'A'
+// EG: getMin(['a', 'b', 'c', 'A', 1, '1']) => '1'
 export const getMin = (arr) => {
-  const out = R.reduce(R.min, Infinity, arr)
-  return out === Infinity ? NaN : out
+  return R.pipe(
+    R.groupBy(R.type),
+    R.cond([
+      [
+        R.pipe(R.prop('String'), R.length, R.flip(R.gt)(0)),
+        R.pipe(
+          R.prop('String'),
+          R.reduce((a, b) => (a < b ? a : b), NaN)
+        ),
+      ],
+      [
+        R.pipe(R.prop('Number'), R.length, R.flip(R.gt)(0)),
+        R.pipe(R.prop('Number'), R.reduce(R.min, Infinity)),
+      ],
+      [R.T, R.always(NaN)],
+    ])
+  )(arr)
 }
 
 // Calculate the mean of a list (returns a number or NaN)
-// This only takes in numbers or returns NaN
+// This omits everything except numbers and returns NaN if there are no numbers
 export const getMean = (arr) => {
-  return R.mean(arr)
+  return R.mean(R.filter(R.is(Number), arr))
 }
 
 // Calculate the median of a list  (returns a number or NaN)
-// This only takes in numbers or returns NaN
+// This omits everything except numbers and returns NaN if there are no numbers
 export const getMedian = (arr) => {
-  return R.median(arr)
+  return R.median(R.filter(R.is(Number), arr))
 }
 
-// Calculate the standard deviation of a list
-// const getStdDev = (arr) => {
+// Calculate the standard deviation of a list (returns a number or NaN)
+// This omits everything except numbers and returns NaN if there are no numbers
+// export const getStdDev = (arr) => {
 //     return R.pipe(
+//         R.filter(R.is(Number)),
 //         R.map(x => Math.pow(x - getMean(arr), 2)),
 //         R.sum,
 //         R.divide(R.__, arr.length),
@@ -55,9 +93,14 @@ export const getMedian = (arr) => {
 // }
 
 // Calculate the sum of a list (returns a number or NaN)
-// This only takes in numbers or returns NaN
+// This omits everything except numbers and returns NaN if there are no numbers
 export const getSum = (arr) => {
-  return R.sum(arr)
+  return R.sum(R.filter(R.is(Number), arr))
+}
+
+// Calculate the count of all items a list (returns a number or NaN)
+export const getCount = (arr) => {
+  return R.length(arr)
 }
 
 // Calculate all statistics as an object given a list
@@ -70,5 +113,6 @@ export const getStats = (arr) => {
     median: getMedian(arr),
     // stdDev: getStdDev(arr),
     sum: getSum(arr),
+    count: getCount(arr),
   }
 }
