MIT-CAVE/cave_static

commit 2c13d8ea2df617de59b99cbd3cb14ae0dbe597aa

Add parent group option to `SelectAccordion` (#132)

src/ui/views/dashboard/DashboardHeaders.js
@@ -33,8 +33,6 @@ import {
 const StatisticsHeader = memo(({ obj, index }) => {
   const categories = useSelector(selectCategoriesData)
   const statisticTypes = useSelector(selectAllowedStats)
-  const sortedStatistics = customSort(statisticTypes)
-  const sortedCategories = customSort(categories)
   const appBarId = useSelector(selectAppBarId)
   const sync = useSelector(selectSync)
   const dispatch = useDispatch()
@@ -46,6 +44,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
   const groupableCategories = R.isNil(groupByOptions)
     ? categories
     : R.pick(groupByOptions, categories)
+  const sortedStatistics = customSort(statisticTypes)
 
   const path = ['dashboards', 'data', appBarId, 'dashboardLayout', index]
   return (
@@ -157,7 +156,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
               iconName: 'MdVerticalAlignTop',
             },
           ]}
-          onSelect={(value) =>
+          onSelect={(value) => {
             dispatch(
               mutateLocal({
                 path,
@@ -165,7 +164,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
                 value: R.assoc('grouping', value, obj),
               })
             )
-          }
+          }}
         />
       </HeaderSelectWrapper>
 
@@ -192,7 +191,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
             value={R.propOr('', 'statistic', obj)}
             placeholder="Statistic"
             optionsList={R.pluck('id')(sortedStatistics)}
-            onSelect={(value) =>
+            onSelect={(value) => {
               dispatch(
                 mutateLocal({
                   path,
@@ -200,7 +199,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
                   value: R.assoc('statistic', value, obj),
                 })
               )
-            }
+            }}
           />
         )}
       </HeaderSelectWrapper>
@@ -210,12 +209,13 @@ const StatisticsHeader = memo(({ obj, index }) => {
             R.props(['category', 'level']),
             R.when(R.any(R.isNil), R.always(''))
           )(obj)}
-          items={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
           placeholder="Group By"
-          subItemLayouts={R.pipe(
-            R.values,
-            R.pluck('layoutDirection')
-          )(sortedCategories)}
+          itemGroups={R.pipe(
+            customSort,
+            R.project(['id', 'grouping', 'layoutDirection', 'order']),
+            R.groupBy(R.prop('grouping'))
+          )(categories)}
+          subItems={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
           getLabel={getLabelFn(categories)}
           getSubLabel={getSubLabelFn(categories)}
           onSelect={(item, subItem) => {
@@ -249,12 +249,13 @@ const StatisticsHeader = memo(({ obj, index }) => {
             R.props(['category2', 'level2']),
             R.when(R.any(R.isNil), R.always(''))
           )(obj)}
-          items={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
           placeholder="Sub Group"
-          subItemLayouts={R.pipe(
-            R.values,
-            R.pluck('layoutDirection')
-          )(sortedCategories)}
+          itemGroups={R.pipe(
+            customSort,
+            R.project(['id', 'grouping', 'layoutDirection', 'order']),
+            R.groupBy(R.prop('grouping'))
+          )(categories)}
+          subItems={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
           getLabel={getLabelFn(categories)}
           getSubLabel={getSubLabelFn(categories)}
           onSelect={(item, subItem) => {
@@ -352,7 +353,7 @@ const KpiHeader = memo(({ obj, index }) => {
             R.project(['id', 'name', 'icon']),
             R.map(renameKeys({ id: 'value', name: 'label', icon: 'iconName' }))
           )(kpis)}
-          onSelect={(value) =>
+          onSelect={(value) => {
             dispatch(
               mutateLocal({
                 path,
@@ -360,7 +361,7 @@ const KpiHeader = memo(({ obj, index }) => {
                 value: R.assoc('kpi', value, obj),
               })
             )
-          }
+          }}
         />
       </HeaderSelectWrapper>
       <HeaderSelectWrapper>

src/ui/compound/SelectAccordion.js
@@ -2,14 +2,15 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Box,
   MenuItem,
   Select,
   Typography,
 } from '@mui/material'
 import PropTypes from 'prop-types'
 import { memo, useState } from 'react'
-import { MdExpandMore } from 'react-icons/md'
 
+import FetchedIcon from './FetchedIcon'
 import OverflowText from './OverflowText'
 
 const styles = {
@@ -24,6 +25,15 @@ const styles = {
   },
   accordionRoot: {
     width: '100%',
+  },
+  accordionGroup: {
+    bgcolor: (theme) =>
+      theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+  },
+  accordionSummary: {
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+      transform: 'rotate(90deg)',
+    },
   },
   getOrientation: (orientation) => ({
     display: 'flex',
@@ -42,11 +52,11 @@ const styles = {
 /**
  * A hybrid of the Select and Accordion components.
  *
- * @param {Array} items
+ * @param disabled
  * @param values
  * @param placeholder
- * @param subItemLayouts
- * @param disabled
+ * @param subItems
+ * @param itemGroups
  * @param getLabel
  * @param getSubLabel
  * @param onClickAway
@@ -55,11 +65,12 @@ const styles = {
  * @private
  */
 const SelectAccordion = ({
-  items,
+  disabled,
   values,
   placeholder,
-  subItemLayouts = [],
-  disabled,
+  subItems,
+  itemGroups = {},
+  groupLabel = 'group',
   getLabel = (label) => label,
   getSubLabel = (label) => label,
   onClickAway = () => {},
@@ -67,6 +78,40 @@ const SelectAccordion = ({
   ...props
 } = {}) => {
   const [open, setOpen] = useState(false)
+
+  const CategoryItems = ({ item, layoutDirection }) => {
+    return (
+      <Accordion
+        key={item}
+        // defaultExpanded
+        sx={styles.accordionRoot}
+        onChange={(event) => {
+          // Prevents other Select components from capturing
+          // the event when expanding/collapsing the accordion
+          event.stopPropagation()
+        }}
+      >
+        <AccordionSummary expandIcon={<FetchedIcon iconName="MdExpandMore" />}>
+          <Typography sx={styles.heading}>{getLabel(item)}</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={styles.getOrientation(layoutDirection)}>
+          {subItems[item].map((subItem, idx) => (
+            <MenuItem
+              key={idx}
+              component="div"
+              onClick={() => {
+                onSelect && onSelect(item, subItem)
+                setOpen(false)
+              }}
+            >
+              {getSubLabel(item, subItem)}
+            </MenuItem>
+          ))}
+        </AccordionDetails>
+      </Accordion>
+    )
+  }
+
   return (
     <Select
       {...{ disabled, open, ...props }}
@@ -103,50 +148,61 @@ const SelectAccordion = ({
           <OverflowText text={placeholder} />
         </MenuItem>
       )}
-
       {/* HACK: Drop warning for non-existing value */}
       {values !== '' && <MenuItem value={values} sx={{ display: 'none' }} />}
+      {/* Render item groups */}
+      {Object.keys(itemGroups).map((grouping, index) => {
+        const grouped = grouping !== 'null' && grouping != null // `grouping != null` is a sanity check
+        const currentItems = itemGroups[grouping] || []
 
-      {Object.keys(items).map((item, index) => (
-        <MenuItem key={index}>
-          <Accordion
-            // defaultExpanded
-            sx={styles.accordionRoot}
-            onChange={(event) => {
-              // Prevents other Select components from capturing
-              // the event when expanding/collapsing the accordion
-              event.stopPropagation()
-            }}
-          >
-            <AccordionSummary expandIcon={<MdExpandMore />}>
-              <Typography sx={styles.heading}>{getLabel(item)}</Typography>
-            </AccordionSummary>
-            <AccordionDetails sx={styles.getOrientation(subItemLayouts[index])}>
-              {items[item].map((subItem, idx) => (
-                <MenuItem
-                  key={idx}
-                  component="div"
-                  onClick={() => {
-                    onSelect && onSelect(item, subItem)
-                    setOpen(false)
-                  }}
+        // Render items per group
+        return (
+          <MenuItem key={index}>
+            {grouped ? (
+              <Accordion
+                // defaultExpanded
+                sx={[styles.accordionRoot, styles.accordionGroup]}
+                onChange={(event) => {
+                  // Prevents other Select components from capturing
+                  // the event when expanding/collapsing the accordion
+                  event.stopPropagation()
+                }}
+              >
+                <AccordionSummary
+                  sx={styles.accordionSummary}
+                  expandIcon={<FetchedIcon iconName="MdOutlineChevronRight" />}
                 >
-                  {getSubLabel(item, subItem)}
-                </MenuItem>
-              ))}
-            </AccordionDetails>
-          </Accordion>
-        </MenuItem>
-      ))}
+                  <Typography sx={styles.heading}>
+                    {`${grouping} `}
+                    <Box component="span" sx={{ color: 'text.secondary' }}>
+                      ({groupLabel})
+                    </Box>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {currentItems.map(({ id: item, layoutDirection }) => (
+                    <CategoryItems {...{ item, layoutDirection }} />
+                  ))}
+                </AccordionDetails>
+              </Accordion>
+            ) : (
+              <CategoryItems
+                item={currentItems[index].id}
+                layoutDirection={currentItems[index].layoutDirection}
+              />
+            )}
+          </MenuItem>
+        )
+      })}
     </Select>
   )
 }
 SelectAccordion.propTypes = {
-  items: PropTypes.object,
+  disabled: PropTypes.bool,
   values: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
   placeholder: PropTypes.string,
-  subItemLayouts: PropTypes.arrayOf(PropTypes.string),
-  disabled: PropTypes.bool,
+  itemGroups: PropTypes.object,
+  subItems: PropTypes.object,
   getLabel: PropTypes.func,
   getSubLabel: PropTypes.func,
   onClickAway: PropTypes.func,

commit ff6fa9f7d61298ab830642ac380304ef47583e34

Fix chart categories bug introduced in #132

src/ui/views/dashboard/DashboardHeaders.js
@@ -46,6 +46,14 @@ const StatisticsHeader = memo(({ obj, index }) => {
     : R.pick(groupByOptions, categories)
   const sortedStatistics = customSort(statisticTypes)
 
+  const subItems = R.mapObjIndexed(getCategoryItems)(groupableCategories)
+  const itemGroups = R.pipe(
+    R.pick(R.keys(subItems)), // Drop any category not included in `nestedStructure`
+    customSort,
+    R.project(['id', 'grouping', 'layoutDirection', 'order']),
+    R.groupBy(R.prop('grouping'))
+  )(categories)
+
   const path = ['dashboards', 'data', appBarId, 'dashboardLayout', index]
   return (
     <>
@@ -210,12 +218,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
             R.when(R.any(R.isNil), R.always(''))
           )(obj)}
           placeholder="Group By"
-          itemGroups={R.pipe(
-            customSort,
-            R.project(['id', 'grouping', 'layoutDirection', 'order']),
-            R.groupBy(R.prop('grouping'))
-          )(categories)}
-          subItems={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
+          {...{ itemGroups, subItems }}
           getLabel={getLabelFn(categories)}
           getSubLabel={getSubLabelFn(categories)}
           onSelect={(item, subItem) => {
@@ -250,12 +253,7 @@ const StatisticsHeader = memo(({ obj, index }) => {
             R.when(R.any(R.isNil), R.always(''))
           )(obj)}
           placeholder="Sub Group"
-          itemGroups={R.pipe(
-            customSort,
-            R.project(['id', 'grouping', 'layoutDirection', 'order']),
-            R.groupBy(R.prop('grouping'))
-          )(categories)}
-          subItems={R.mapObjIndexed(getCategoryItems)(groupableCategories)}
+          {...{ itemGroups, subItems }}
           getLabel={getLabelFn(categories)}
           getSubLabel={getSubLabelFn(categories)}
           onSelect={(item, subItem) => {