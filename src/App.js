40c762d81652de00c2384a398a67d82324c94b08
aiizhao <alzhao72@gmail.com>
2023-08-03 12:49:19 -0400

    clean App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import { useSelector } from 'react-redux'
 import {
   selectAppBarId,
   selectAppBarData,
-  selectLeftAppBarDisplay,
-  selectRightAppBarDisplay,
   selectMapboxToken,
   selectTheme,
 } from './data/selectors'
@@ -54,18 +52,12 @@ const App = () => {
   const themeId = useSelector(selectTheme)
   const appBarId = useSelector(selectAppBarId)
   const appBarData = useSelector(selectAppBarData)
-  const leftBar = useSelector(selectLeftAppBarDisplay)
-  const rightBar = useSelector(selectRightAppBarDisplay)
 
   const theme = getTheme(themeId)
   const renderAppPage = R.cond([
     [
       R.equals(viewId.MAP),
-      R.always(
-        mapboxToken && (leftBar || rightBar) ? (
-          <Map {...{ mapboxToken }} />
-        ) : null
-      ),
+      R.always(mapboxToken ? <Map {...{ mapboxToken }} /> : null),
     ],
     [R.equals(viewId.DASHBOARD), R.always(<Dashboard />)],
     [R.equals(viewId.KPI), R.always(<Kpi />)],
