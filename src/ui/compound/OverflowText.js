df53cc2c1b902ab925f82d5cd79724e62fb94013
mrkwanzaa <sledgeguter@gmail.com>
2023-11-14 15:04:04 -0500

    Fix OverflowText not updating (#360)

--- src/ui/compound/OverflowText.js
+++ src/ui/compound/OverflowText.js
@@ -1,6 +1,12 @@
 import { Box } from '@mui/material'
 import PropTypes from 'prop-types'
-import React, { useCallback, useState } from 'react'
+import React, {
+  useCallback,
+  useState,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+} from 'react'
 import Marquee from 'react-fast-marquee'
 
 import { forceArray } from '../../utils'
@@ -17,29 +23,52 @@ const styles = {
 
 const OverflowText = ({ text, speed = 20, sx = [], children, ...props }) => {
   const [isOverflowing, setIsOverflowing] = useState(false)
+  const checkedFalse = useRef(false)
+  const parent = useRef(null)
+  const node = useRef(null)
 
-  const divRef = useCallback((node) => {
-    if (node == null) return
-
+  const checkOverflow = useCallback((node) => {
+    if (node === null) return
     const overflowX = node.offsetWidth < node.scrollWidth
     const overflowY = node.offsetHeight < node.scrollHeight
     setIsOverflowing(overflowX || overflowY)
   }, [])
+
+  // if the text or parent size changes, check again for overflow
+  useLayoutEffect(() => {
+    checkedFalse.current = false
+  }, [text, parent.offsetWidth, parent.offsetHeight])
+
+  // This useEffect is needed to check if the text is overflowing.
+  useEffect(() => {
+    // if the non-scrolling text isn't rendered, try and see if it overflows
+    if (node.current.scrollWidth === 0) {
+      if (!checkedFalse.current) {
+        // we only need to try once so keep track of that here
+        checkedFalse.current = true
+        setIsOverflowing(false)
+      }
+      return
+    }
+    // if the non-scrolling text is rendered, check if it overflows
+    checkOverflow(node.current)
+  }, [text, checkOverflow, isOverflowing])
+
   return (
-    <Box sx={[styles.root, ...forceArray(sx)]} ref={divRef} {...props}>
-      {isOverflowing ? (
-        <Marquee
-          direction="left"
-          delay={0.5}
-          gradient={false}
-          pauseOnHover
-          {...{ speed }}
-        >
-          <Box sx={styles.marqueeContent}>{children || text}</Box>
-        </Marquee>
-      ) : (
-        <>{children || text}</>
-      )}
+    <Box sx={[styles.root, ...forceArray(sx)]} {...props} useRef={parent}>
+      <Marquee
+        direction="left"
+        delay={0.5}
+        gradient={false}
+        pauseOnHover
+        {...{ speed }}
+        style={{ display: isOverflowing ? '' : 'none' }}
+      >
+        <Box sx={styles.marqueeContent}>{children || text}</Box>
+      </Marquee>
+      <Box style={{ display: isOverflowing ? 'none' : '' }} ref={node}>
+        {children || text}
+      </Box>
     </Box>
   )
 }
