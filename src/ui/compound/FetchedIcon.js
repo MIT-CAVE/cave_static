c87ac6cd52eb394dcc4877caee7e0738973c20bb
mrkwanzaa <sledgeguter@gmail.com>
2024-04-24 10:51:11 -0400

    Change loading placeholder icon

--- src/ui/compound/FetchedIcon.js
+++ src/ui/compound/FetchedIcon.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import { useState, useEffect } from 'react'
-import { MdDownloading } from 'react-icons/md'
+import { BiSolidSquareRounded } from 'react-icons/bi'
 import { useSelector } from 'react-redux'
 
 import { selectSettingsIconUrl } from '../../data/selectors'
@@ -9,7 +9,7 @@ import { fetchIcon, addExtraProps, removeExtraProps } from '../../utils'
 
 const FetchedIcon = ({ iconName = 'md/MdDownloading', ...props }) => {
   const iconUrl = useSelector(selectSettingsIconUrl)
-  const [icon, setIcon] = useState(<MdDownloading />)
+  const [icon, setIcon] = useState(<BiSolidSquareRounded />)
   useEffect(() => {
     fetchIcon(iconName, iconUrl).then((item) => setIcon(item))
   }, [iconName, iconUrl])
