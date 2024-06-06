f544fcf34ffadcd8751fb086139877a91547ac35
Alexander Yang <alex@Alexanders-MBP.fios-router.home>
2024-06-06 14:06:46 -0400

    changed incremental slider

--- src/ui/compound/PropIncSlider.js
+++ src/ui/compound/PropIncSlider.js
@@ -1,7 +1,7 @@
 import { Box, Slider } from '@mui/material'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { forceArray } from '../../utils'
 
@@ -20,33 +20,30 @@ const getStyles = (enabled) => ({
   },
 })
 
-// const valueToIndex = (val, min, step) => (val - min) / step
 const PropIncSlider = ({ prop, sx = [], ...props }) => {
-  const { enabled = false } = prop
+  const { enabled = false, options } = prop
   const [value, setValue] = useState(prop.value)
-  const min = R.propOr(-Infinity, 'minValue', prop)
-  const max = R.propOr(Infinity, 'maxValue', prop)
-  const step = prop['stepValue']
-  const IndexToValue = (ind) => ind * step + min
-  const options = R.map(IndexToValue, R.range(0, (max - min) / step + 1))
-  useEffect(() => {
-    setValue(value)
-  }, [value, options])
-
+  const marks = R.map(
+    (x) => ({
+      value: x,
+      label: options[x],
+    }),
+    R.range(0, R.length(options))
+  )
   return (
     <Box sx={[getStyles(enabled).box, ...forceArray(sx)]} {...props}>
       <Slider
         style={getStyles(enabled).slider}
         min={0}
-        max={max}
-        step={step}
+        max={R.length(options) - 1}
+        step={null}
         track={false}
         disabled={!enabled}
         valueLabelDisplay="off"
-        value={value}
-        marks={R.map((x) => ({ value: x, label: x }), options)}
+        value={R.indexOf(value, options)}
+        marks={marks}
         onChange={(_, val) => {
-          if (enabled) setValue(val)
+          if (enabled) setValue(options[val])
         }}
       />
     </Box>
