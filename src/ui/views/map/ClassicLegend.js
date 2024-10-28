b7db35bdfd1434d83e0e82e141e0daf6aa643c25
Ella Wang <ellawang9@gmail.com>
2024-10-28 12:02:43 -0400

    updating width of dropdown (#559)

--- src/ui/views/map/ClassicLegend.js
+++ src/ui/views/map/ClassicLegend.js
@@ -547,7 +547,7 @@ const MapLegendSizeBySection = ({
         size={12}
         spacing={numberFormatProps.unit ? 0.5 : 0}
       >
-        <Grid2 size={7}>
+        <Grid2 size={8}>
           <SimpleDropdown
             paperProps={{ elevation: 3 }}
             marquee
@@ -716,7 +716,7 @@ const MapLegendColorBySection = ({
         size={12}
         spacing={numberFormatProps.unit ? 0.5 : 0}
       >
-        <Grid2 size={7}>
+        <Grid2 size={8}>
           <SimpleDropdown
             paperProps={{ elevation: 3 }}
             marquee
@@ -852,7 +852,7 @@ const MapLegendHeightBySection = ({
         size={12}
         spacing={numberFormatProps.unit ? 0.5 : 0}
       >
-        <Grid2 size={7}>
+        <Grid2 size={8}>
           <SimpleDropdown
             paperProps={{ elevation: 3 }}
             marquee
