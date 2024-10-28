2f00798b45aeb32de21870d4f7371f173ab2ebc4
Ella Wang <ellawang9@gmail.com>
2024-10-28 12:08:09 -0400

    modifying max height (#556)

--- src/ui/views/map/MapModal.js
+++ src/ui/views/map/MapModal.js
@@ -64,7 +64,7 @@ const styles = {
     right: '64px',
     bottom: '72px',
     width: '280px',
-    maxHeight: '50%',
+    maxHeight: 'calc(100% - 100px)',
     overflowY: 'auto',
     border: 1,
     borderColor: 'text.secondary',
