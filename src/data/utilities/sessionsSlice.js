4b76eb3a9e82619393d9eb95b354a10b55fe9844
Connor Makowski <connorman528@gmail.com>
2025-02-07 15:50:06 -0500

    Clear versions on session change.

--- src/data/utilities/sessionsSlice.js
+++ src/data/utilities/sessionsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import * as R from 'ramda'
 
-// import { clearVersions } from '../data'
+import { clearVersions } from '../data'
 
 export const sessionsSlice = createSlice({
   name: 'sessions',
@@ -13,9 +13,9 @@ export const sessionsSlice = createSlice({
     // Update sessions from ws message
     updateSessions: (state, action) => {
       // Clear versions if session_id changes to prevent id colisions
-      // if (R.equals(action.payload.data_path, ['session_id'])) {
-      //   action.asyncDispatch(clearVersions())
-      // }
+      if (R.equals(action.payload.data_path, ['session_id'])) {
+        action.asyncDispatch(clearVersions())
+      }
       return R.assocPath(action.payload.data_path, action.payload.data, state)
     },
   },
