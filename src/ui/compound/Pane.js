67c34752a98ef630f6a8aa005d6c35b600b913e8
Luis Vasquez <luisvasq@mit.edu>
2025-05-01 03:30:09 +0200

    Fix horizontal alignment of pane header

--- src/ui/compound/Pane.js
+++ src/ui/compound/Pane.js
@@ -1,6 +1,7 @@
-import { Box, Drawer, IconButton } from '@mui/material'
+import { Box, Drawer, IconButton, Stack, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
+import { MdPushPin, MdOutlinePushPin } from 'react-icons/md'
 
 import FetchedIcon from './FetchedIcon'
 
@@ -31,10 +32,6 @@ const styles = {
     fontSize: '25px',
     borderColor: 'text.secondary',
     borderBottom: '2px',
-  },
-  title: {
-    ml: 0.5,
-    mr: 0.5,
   },
   leftButton: {
     ml: 0.5,
@@ -98,7 +95,6 @@ const PaneHeader = ({
   sx = [],
   ...props
 }) => {
-  const icon = <FetchedIcon {...{ iconName }} />
   const pinButton = (
     <IconButton
       sx={[
@@ -108,16 +104,17 @@ const PaneHeader = ({
       ]}
       onClick={onPin}
     >
-      <FetchedIcon iconName={pin ? 'md/MdPushPin' : 'md/MdOutlinePushPin'} />
+      {pin ? <MdPushPin /> : <MdOutlinePushPin />}
     </IconButton>
   )
   return (
     <Box sx={[styles.header, ...forceArray(sx)]} {...props}>
       {pin != null && side === 'right' && pinButton}
       <Box sx={styles.leftButton}>{leftButton}</Box>
-      <Box sx={styles.title}>
-        {title} {icon}
-      </Box>
+      <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+        <Typography variant="h6">{title}</Typography>
+        <FetchedIcon {...{ iconName }} />
+      </Stack>
       <Box sx={styles.rightButton}>{rightButton}</Box>
       {pin != null && side === 'left' && pinButton}
     </Box>
