917e31e1717de2e95acd0446a4d3439355d4bbd7
Luis Vasquez <luisvasq@mit.edu>
2025-10-20 03:17:24 +0200

    Update build tool from CRA to Vite & configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+/**
+ * @see https://github.com/lint-staged/lint-staged
+ */
+const config = {
   '*.{json,css,html,md}': 'prettier --write',
   '*.{js,jsx}': ['prettier --write', 'eslint --max-warnings=0 --fix'],
 }
+
+export default config
