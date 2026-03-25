b5573febbf620574e199d64d4936469553bd16bc
Luis Vasquez <luisvasq@mit.edu>
2026-03-26 00:18:17 +0100

    Add number formatting to `NumberField` (#656)

--- src/ui/prototypes/NumberField.js
+++ src/ui/prototypes/NumberField.js
@@ -1,5 +1,6 @@
 // Hybrid prototype (Base UI + MUI) for CAVE's next Design System,
 // intended to be built purely on Base UI (release target: v5+)
+import { Field } from '@base-ui/react/field'
 import { NumberField as BaseNumberField } from '@base-ui/react/number-field'
 import {
   FormControl,
@@ -16,11 +17,7 @@ import Spinner, {
   SpinnerIncreaseButton,
 } from './Spinner'
 
-import {
-  forceArray,
-  getStatusIcon,
-  // NumberFormat
-} from '../../utils'
+import { forceArray, getStatusIcon, NumberFormat } from '../../utils'
 
 const styles = {
   adornment: {
@@ -35,6 +32,7 @@ const styles = {
 
 const NumberField = ({
   id: idProp,
+  name,
   disabled,
   error,
   readOnly,
@@ -43,7 +41,7 @@ const NumberField = ({
   value,
   min,
   max,
-  // numberFormat,
+  numberFormat: numberFormatRaw,
   color = 'default',
   helperText,
   sx = [],
@@ -74,16 +72,13 @@ const NumberField = ({
   const handleValueCommitted = (newValue, event) => {
     if (disabled || readOnly) return
 
-    // REVIEW: Perf is degraded after `NumberFormat.format`
-    // onChangeCommitted(event, NumberFormat.format(newValue, numberFormat))
     const clampedValue = Math.min(max, Math.max(min, newValue))
     onChangeCommitted(event, clampedValue)
   }
+
   return (
-    <BaseNumberField.Root
-      onValueChange={handleValueChange}
-      onValueCommitted={onChangeCommitted ? handleValueCommitted : undefined}
-      {...{ value, ...rest }}
+    <Field.Root
+      {...{ name }}
       render={(props) => (
         <FormControl
           ref={props.ref}
@@ -94,104 +89,132 @@ const NumberField = ({
         </FormControl>
       )}
     >
-      <InputLabel htmlFor={id}>{label}</InputLabel>
-      <BaseNumberField.Input
-        render={(props, state) => {
-          console.log(state.inputValue, typeof state.inputValue)
-          return (
-            <OutlinedInput
-              {...{ id }}
-              inputRef={props.ref}
-              value={state.inputValue}
-              // value={NumberFormat.format(state.inputValue, numberFormat)}
-              {...{ label, color, placeholder, readOnly }}
-              sx={[
-                spinner === 'left' && { pl: 0 },
-                spinner === 'right' && { pr: 0 },
-                spinner === 'leftAndRight' && { px: 0 },
-              ]}
-              slotProps={{
-                ...slotProps,
-                input: {
-                  ...props,
-                  ...slotProps?.input,
-                  sx: [
-                    spinner === 'leftAndRight' && { textAlign: 'center' },
-                    ...forceArray(slotProps?.input?.sx),
-                  ],
-                },
-              }}
-              startAdornment={
-                <InputAdornment
-                  position="start"
-                  sx={[
-                    spinner && styles.adornment,
-                    spinner === 'left' && { mr: '14px' },
-                  ]}
-                >
-                  {spinner === 'left' ? (
-                    <Spinner side="left" {...{ decreaseIcon, increaseIcon }} />
-                  ) : spinner === 'leftAndRight' ? (
-                    <SpinnerDecreaseButton
-                      // {...{ size }}
-                      icon={decreaseIcon}
-                      sx={[
-                        {
-                          borderRight: '1px solid',
-                          // boxShadow: 'inset -1px 0 0 rgb(255 255 255 / .12)',
-                        },
-                        size === 'medium' && { px: 1.5 },
-                        styles.spinnerButton,
-                      ]}
-                    />
-                  ) : null}
-                </InputAdornment>
-              }
-              endAdornment={
-                <InputAdornment
-                  position="end"
-                  sx={[
-                    spinner && styles.adornment,
-                    spinner === 'right' && { ml: '14px' },
-                  ]}
-                >
-                  {endAdornments}
-                  {spinner === 'right' || spinner === true ? (
-                    <Spinner side="right" {...{ decreaseIcon, increaseIcon }} />
-                  ) : spinner === 'leftAndRight' ? (
-                    <SpinnerIncreaseButton
-                      icon={increaseIcon}
-                      // {...{ size }}
-                      sx={[
-                        {
-                          borderLeft: '1px solid',
-                          // boxShadow: 'inset 1px 0 0 rgb(255 255 255 / .12)',
-                        },
-                        size === 'medium' && { px: 1.5 },
-                        styles.spinnerButton,
-                      ]}
-                    />
-                  ) : null}
-                  {color !== 'default' && statusIcon && getStatusIcon(color)}
-                </InputAdornment>
-              }
-              onChange={props.onChange}
-              onSelect={props.onSelect}
-              onFocus={props.onFocus}
-              onBlur={props.onBlur}
-              onTouchStart={props.onTouchStart}
-              onTouchMove={props.onTouchMove}
-              onTouchEnd={props.onTouchEnd}
-              onKeyUp={props.onKeyUp}
-              onKeyDown={props.onKeyDown}
-            />
-          )
-        }}
-      />
-      <FormHelperText sx={{ ml: 0, '&:empty': { mt: 0 } }}>
-        {helperText}
-      </FormHelperText>
-    </BaseNumberField.Root>
+      <BaseNumberField.Root
+        onValueChange={handleValueChange}
+        onValueCommitted={onChangeCommitted ? handleValueCommitted : undefined}
+        {...{ value, min, max, readOnly, ...rest }}
+        style={{ width: fullWidth ? '100%' : 'auto' }}
+      >
+        <InputLabel htmlFor={id}>{label}</InputLabel>
+        <BaseNumberField.Input
+          render={(props, state) => {
+            // Here, units are excluded from `format` as
+            // they are rendered in the prop container
+            // eslint-disable-next-line no-unused-vars
+            const { unit, unitPlacement, ...numberFormat } = numberFormatRaw
+            const formattedValue = NumberFormat.format(
+              state.value,
+              numberFormat
+            )
+            const inputRef = props.ref
+            return (
+              <OutlinedInput
+                {...{
+                  id,
+                  inputRef,
+                  label,
+                  color,
+                  placeholder,
+                  readOnly,
+                  fullWidth,
+                }}
+                value={state.inputValue}
+                sx={[
+                  spinner === 'left' && { pl: 0 },
+                  spinner === 'right' && { pr: 0 },
+                  spinner === 'leftAndRight' && { px: 0 },
+                ]}
+                slotProps={{
+                  ...slotProps,
+                  input: {
+                    ...props,
+                    // Display the formatted value using our `NumberFormat.format` function
+                    value: state.focused ? state.value : formattedValue,
+                    ...slotProps?.input,
+                    sx: [
+                      spinner === 'leftAndRight' && { textAlign: 'center' },
+                      ...forceArray(slotProps?.input?.sx),
+                    ],
+                  },
+                }}
+                startAdornment={
+                  <InputAdornment
+                    position="start"
+                    sx={[
+                      spinner && styles.adornment,
+                      spinner === 'left' && { mr: '14px' },
+                    ]}
+                  >
+                    {spinner === 'left' ? (
+                      <Spinner
+                        side="left"
+                        {...{ decreaseIcon, increaseIcon }}
+                      />
+                    ) : spinner === 'leftAndRight' ? (
+                      <SpinnerDecreaseButton
+                        // {...{ size }}
+                        icon={decreaseIcon}
+                        sx={[
+                          {
+                            borderRight: '1px solid',
+                            // boxShadow: 'inset -1px 0 0 rgb(255 255 255 / .12)',
+                          },
+                          size === 'medium' && { px: 1.5 },
+                          styles.spinnerButton,
+                        ]}
+                      />
+                    ) : null}
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <InputAdornment
+                    position="end"
+                    sx={[
+                      spinner && styles.adornment,
+                      spinner === 'right' && { ml: '14px' },
+                    ]}
+                  >
+                    {endAdornments}
+                    {spinner === 'right' || spinner === true ? (
+                      <Spinner
+                        side="right"
+                        {...{ decreaseIcon, increaseIcon }}
+                      />
+                    ) : spinner === 'leftAndRight' ? (
+                      <SpinnerIncreaseButton
+                        icon={increaseIcon}
+                        // {...{ size }}
+                        sx={[
+                          {
+                            borderLeft: '1px solid',
+                            // boxShadow: 'inset 1px 0 0 rgb(255 255 255 / .12)',
+                          },
+                          size === 'medium' && { px: 1.5 },
+                          styles.spinnerButton,
+                        ]}
+                      />
+                    ) : null}
+                    {color !== 'default' && statusIcon && getStatusIcon(color)}
+                  </InputAdornment>
+                }
+                onChange={props.onChange}
+                onSelect={props.onSelect}
+                onFocus={props.onFocus}
+                onBlur={props.onBlur}
+                onTouchStart={props.onTouchStart}
+                onTouchMove={props.onTouchMove}
+                onTouchEnd={props.onTouchEnd}
+                onKeyUp={props.onKeyUp}
+                onKeyDown={props.onKeyDown}
+              />
+            )
+          }}
+        />
+        <FormHelperText sx={{ ml: 0, '&:empty': { mt: 0 } }}>
+          {helperText}
+        </FormHelperText>
+      </BaseNumberField.Root>
+    </Field.Root>
   )
 }
 
@@ -199,8 +222,8 @@ NumberField.propTypes = {
   id: PropTypes.string,
   label: PropTypes.node,
   spinner: PropTypes.oneOf([false, true, 'right', 'left', 'leftAndRight']),
-  // error: PropTypes.bool,
-  // size: PropTypes.oneOf(['medium', 'small']),
+  error: PropTypes.bool,
+  size: PropTypes.oneOf(['medium', 'small']),
   onClickAway: PropTypes.func.isRequired,
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
